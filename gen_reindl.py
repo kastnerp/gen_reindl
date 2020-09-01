e54186129e827fab99eba2ba1087dcc6c4bac529
Patrick Kastner <patrick.kastner@gmail.com>
2020-09-01 15:55:12 -0400

    Added proper class and unit test

--- gen_reindl.py
+++ gen_reindl.py
@@ -1,189 +1,210 @@
-
 import math
 
-# gen_reindl:
-# Program that transforms global irradiances into orizontal diffuse and direct normal irradiances
-# Note that the -o option has to be specified!
-# Note that the -i option has to be specified!
-#
-# Supported options are:
-# -i       input file [format: month day hour global_irradiation
-# -o       output file [format: month day hour dir_norm_irrad dif_hor_irrad
-# -m       time zone
-# -l       longitude [DEG, West is positive]
-# -a       latitude [DEG, North is positive]
+
+class GenReindl:
+    """
+    Python port of 'gen_reindl.exe', a program that transforms global irradiances into orizontal diffuse and direct normal irradiances.
+
+    ...
 
 
-solar_constant_e = 1367.0
-DTR = 0.017453292  # Pi / 180
-RTD = 57.2957795  # 180 / Pi
+    Methods
+    -------
+    calc_split(month, day, time, irrad_glo)
+        Calculates a tuple of direct normal irradiation and diffuse horizontal irradiation
+    """
 
+    def __init__(self, lat, lon, time_zone):
 
-def month_and_day_to_julian_day(month, day):
-    if (month == 1):  return day
-    if (month == 2):  return day + 31
-    if (month == 3):  return day + 59
-    if (month == 4):  return day + 90
-    if (month == 5):  return day + 120
-    if (month == 6):  return day + 151
-    if (month == 7):  return day + 181
-    if (month == 8):  return day + 212
-    if (month == 9):  return day + 243
-    if (month == 10):  return day + 273
-    if (month == 11):  return day + 304
-    if (month == 12):
-        return day + 334
-    else:
-        print("bad month")
+        """
+        Parameters
+        ----------
+        lat  : float
+            latitude (north positive)
+        lon : float
+            longitude (west positive)
+        time_zone  : int
+            Time zone in a multiple of 15 degrees from the meridian. Example: for Singapore, UTC+8 * 15 = -120, despite a -103.98 longitude.
+        """
 
+        self.solar_constant_e = 1367.0
+        self.DTR = 0.017453292  # Pi / 180
+        self.RTD = 57.2957795  # 180 / Pi
+        self.lat = lat
+        self.lon = lon
+        self.time_zone = time_zone
 
-def solar_elev_azi_ecc(latitude, longitude, time_zone, jday, time, solar_time=0):
-    # /*  angles in degrees, times in hours  */
+        self.time = None
+        self.month = None
+        self.day = None
+        self.irrad_beam_nor = None
+        self.irrad_dif = None
 
-    # /*  solar elevation and azimuth formulae from sun.c  */
-    if (solar_time == 1):   sol_time = time
-    if (solar_time == 0):   sol_time = time + 0.170 * math.sin((4 * math.pi / 373) * (jday - 80)) - 0.129 * math.sin((2 * math.pi / 355) * (jday - 8)) + 12 / 180.0 * (
-            time_zone - longitude)
+    def month_and_day_to_julian_day(self, month, day):
+        if month == 1:
+            return day
+        if month == 2:
+            return day + 31
+        if month == 3:
+            return day + 59
+        if month == 4:
+            return day + 90
+        if month == 5:
+            return day + 120
+        if month == 6:
+            return day + 151
+        if month == 7:
+            return day + 181
+        if month == 8:
+            return day + 212
+        if month == 9:
+            return day + 243
+        if month == 10:
+            return day + 273
+        if month == 11:
+            return day + 304
+        if month == 12:
+            return day + 334
+        else:
+            print("bad month")
 
-    solar_declination = RTD * 0.4093 * math.sin((2 * math.pi / 368) * (jday - 81))
-    jday_angle = 2 * math.pi * (jday - 1) / 365
+    def solar_elev_azi_ecc(self, latitude,
+                           longitude,
+                           time_zone,
+                           jday,
+                           time,
+                           solar_time=0):
+        # /*  angles in degrees, times in hours  */
 
-    solar_elevation = RTD * math.asin(
-        math.sin(latitude * DTR) * math.sin(solar_declination * DTR) - math.cos(latitude * DTR) * math.cos(solar_declination * DTR) * math.cos(sol_time * (math.pi / 12)))
+        # /*  solar elevation and azimuth formulae from sun.c  */
+        if solar_time == 1:
+            sol_time = time
+        if solar_time == 0:
+            sol_time = time + 0.170 * math.sin(
+                (4 * math.pi / 373) * (jday - 80)) - 0.129 * math.sin(
+                (2 * math.pi / 355) *
+                (jday - 8)) + 12 / 180.0 * (time_zone - longitude)
 
-    solar_azimuth = RTD * (-math.atan2(math.cos(solar_declination * DTR) * math.sin(sol_time * (math.pi / 12)),
-                                       -math.cos(latitude * DTR) * math.sin(solar_declination * DTR) -
-                                       math.sin(latitude * DTR) * math.cos(solar_declination * DTR) * math.cos(sol_time * (math.pi / 12))))
+        solar_declination = self.RTD * 0.4093 * math.sin(
+            (2 * math.pi / 368) * (jday - 81))
+        jday_angle = 2 * math.pi * (jday - 1) / 365
 
-    # /*  eccentricity_correction formula used in genjdaylit.c */
+        solar_elevation = self.RTD * math.asin(
+            math.sin(latitude * self.DTR) * math.sin(solar_declination * self.DTR) -
+            math.cos(latitude * self.DTR) * math.cos(solar_declination * self.DTR) *
+            math.cos(sol_time * (math.pi / 12)))
 
-    eccentricity_correction = 1.00011 + 0.034221 * math.cos(jday_angle) + 0.00128 * math.sin(jday_angle) + 0.000719 * math.cos(2 * jday_angle) + 0.000077 * math.sin(2 * jday_angle)
+        solar_azimuth = self.RTD * (-math.atan2(
+            math.cos(solar_declination * self.DTR) * math.sin(sol_time *
+                                                              (math.pi / 12)),
+            -math.cos(latitude * self.DTR) * math.sin(solar_declination * self.DTR) -
+            math.sin(latitude * self.DTR) * math.cos(solar_declination * self.DTR) *
+            math.cos(sol_time * (math.pi / 12))))
 
-    return solar_elevation, solar_azimuth, eccentricity_correction
+        # /*  eccentricity_correction formula used in genjdaylit.c */
 
+        eccentricity_correction = 1.00011 + 0.034221 * math.cos(
+            jday_angle) + 0.00128 * math.sin(jday_angle) + 0.000719 * math.cos(
+            2 * jday_angle) + 0.000077 * math.sin(2 * jday_angle)
 
-def diffuse_fraction(irrad_glo, solar_elevation, eccentricity_correction):
-    dif_frac = 0
+        return solar_elevation, solar_azimuth, eccentricity_correction
 
-    if (solar_elevation > 0):
-        irrad_ex = solar_constant_e * eccentricity_correction * math.sin(DTR * solar_elevation)
-    else:
-        irrad_ex = 0
+    def diffuse_fraction(self, irrad_glo, solar_elevation, eccentricity_correction):
+        dif_frac = 0
 
-    if (irrad_ex > 0):
-        index_glo_ex = irrad_glo / irrad_ex
-    else:
-        return 0
+        if solar_elevation > 0:
+            irrad_ex = self.solar_constant_e * eccentricity_correction * math.sin(
+                self.DTR * solar_elevation)
+        else:
+            irrad_ex = 0
 
-    if (index_glo_ex < 0):  print("negative irrad_glo in diffuse_fraction_th\n")
-    if (index_glo_ex > 1):  index_glo_ex = 1
+        if irrad_ex > 0:
+            index_glo_ex = irrad_glo / irrad_ex
+        else:
+            return 0
 
-    if (index_glo_ex <= 0.3):
-        dif_frac = 1.02 - 0.254 * index_glo_ex + 0.0123 * math.sin(DTR * solar_elevation)
-    if (dif_frac > 1):
-        dif_frac = 1
+        if index_glo_ex < 0:
+            print("negative irrad_glo in diffuse_fraction_th\n")
+        if index_glo_ex > 1:
+            index_glo_ex = 1
 
-    if (0.3 < index_glo_ex < 0.78):
-        dif_frac = 1.4 - 1.749 * index_glo_ex + 0.177 * math.sin(DTR * solar_elevation)
-    if (dif_frac > 0.97):
-        dif_frac = 0.97
-    if (dif_frac < 0.1):   dif_frac = 0.1
+        if index_glo_ex <= 0.3:
+            dif_frac = 1.02 - 0.254 * index_glo_ex + 0.0123 * math.sin(
+                self.DTR * solar_elevation)
+        if dif_frac > 1:
+            dif_frac = 1
 
-    if (index_glo_ex >= 0.78):
-        dif_frac = 0.486 * index_glo_ex - 0.182 * math.sin(DTR * solar_elevation)
-    if dif_frac < 0.1:
-        dif_frac = 0.1
+        if 0.3 < index_glo_ex < 0.78:
+            dif_frac = 1.4 - 1.749 * index_glo_ex + 0.177 * math.sin(self.DTR * solar_elevation)
+        if dif_frac > 0.97:
+            dif_frac = 0.97
+        if dif_frac < 0.1:
+            dif_frac = 0.1
 
-    return dif_frac
+        if index_glo_ex >= 0.78:
+            dif_frac = 0.486 * index_glo_ex - 0.182 * math.sin(
+                self.DTR * solar_elevation)
+        if dif_frac < 0.1:
+            dif_frac = 0.1
 
+        return dif_frac
 
-def calc_split(month, day, time, irrad_glo):
-    jday = month_and_day_to_julian_day(month, day)
+    def calc_split(self, month, day, time, irrad_glo):
 
-    if (irrad_glo < 0 or irrad_glo > solar_constant_e):  # / * check irradiances and exit if necessary * /
-        irrad_glo = solar_constant_e
+        """Calculates a tuple of direct normal irradiation and diffuse horizontal irradiation
 
-    # no object orientation yet
+        Parameters
+        ----------
+        month : int
+            month
+        day : int
+            day
+        time : float
+            hour of the day
+        irrad_glo :
+            Global irradiation from e.g. a weather station
 
-    solar_elevation, solar_azimuth, eccentricity_correction = solar_elev_azi_ecc(lat, lon, time_zone, jday, time)
+        Returns
+        -------
+        tuple
+            Direct normal irradiation and diffuse horizontal irradiation
+        """
 
-    #
+        jday = self.month_and_day_to_julian_day(month, day)
 
-    irrad_dif = diffuse_fraction(irrad_glo, solar_elevation, eccentricity_correction) * irrad_glo
+        if irrad_glo < 0 or irrad_glo > self.solar_constant_e:  # / * check irradiances and exit if necessary * /
+            irrad_glo = self.solar_constant_e
 
-    if (solar_elevation > 5.0):
+        # no object orientation yet
 
-        irrad_beam_nor = (irrad_glo - irrad_dif) * 1.0 / math.sin(DTR * solar_elevation)
+        solar_elevation, solar_azimuth, eccentricity_correction = self.solar_elev_azi_ecc(
+            self.lat, self.lon, self.time_zone, jday, time)
 
-    else:
-        irrad_beam_nor = 0
-        irrad_dif = irrad_glo
+        #
 
-    if (irrad_beam_nor > solar_constant_e):
-        irrad_beam_nor = solar_constant_e
-        irrad_dif = irrad_glo - irrad_beam_nor * math.sin(DTR * solar_elevation)
+        irrad_dif = self.diffuse_fraction(irrad_glo, solar_elevation,
+                                          eccentricity_correction) * irrad_glo
 
-    print(month, day, round(time, 2), round(irrad_beam_nor, 2), round(irrad_dif, 2))
+        if solar_elevation > 5.0:
 
-    return irrad_beam_nor, irrad_dif
+            irrad_beam_nor = (irrad_glo - irrad_dif) * 1.0 / math.sin(
+                self.DTR * solar_elevation)
 
+        else:
+            irrad_beam_nor = 0
+            irrad_dif = irrad_glo
 
-lon = -103.98
-lat = 1.37
-time_zone = -120
+        if irrad_beam_nor > self.solar_constant_e:
+            irrad_beam_nor = self.solar_constant_e
+            irrad_dif = irrad_glo - irrad_beam_nor * math.sin(
+                self.DTR * solar_elevation)
 
-print("month, day, time, irrad_beam_nor, irrad_dif\n")
-calc_split(4, 22, 8.333333333, 107)
-calc_split(4, 22, 8.416666667, 121)
-calc_split(4, 22, 8.5, 137)
-calc_split(4, 22, 8.583333333, 151)
-calc_split(4, 22, 8.666666667, 159)
-calc_split(4, 22, 8.75, 169)
-calc_split(4, 22, 8.833333333, 178)
-calc_split(4, 22, 8.916666667, 184)
+        # print(month, day, round(time, 2), round(irrad_beam_nor, 2), round(irrad_dif, 2))
 
-# http://onebuilding.org/archive/bldg-sim-onebuilding.org/2015-May/046325.html
+        self.time = time
+        self.month = month
+        self.day = day
+        self.irrad_beam_nor = irrad_beam_nor
+        self.irrad_dif = irrad_dif
 
-# Hi Phil,
-#
-# The gen_reindl program that comes with Daysim <http://daysim.ning.com/>
-# does this pretty well, although it isn't documented on the Daysim
-# website. Below is some info I sent to a student using global horizontal
-# irradiation from our weather station for daylight simulation purposes.
-#
-# First you need to create a tab separated text file of the format *m d
-# h(decimal)    gh_irrad* like the below.
-#
-#     4    22    8.333333333    107
-#     4    22    8.416666667    121
-#     4    22    8.5    137
-#     4    22    8.583333333    151
-#     4    22    8.666666667    159
-#     4    22    8.75    169
-#     4    22    8.833333333    178
-#     4    22    8.916666667    184
-#
-# Then the gen_reindl program can be run. -l is longitude (west positive),
-# -a is latitude (north positive) and -m is the time zone in a multiple of
-# 15 degrees from the meridian. The command below is for Singapore, and
-# you note that it is in the wrong time zone. UTC+8 * 15 = -120, despite a
-# -103.98 longitude.
-#  > gen_reindl -m -120 -l -103.98 -a 1.37 -i input.txt -o output.wea
-#
-# After running the command, the output in the output.wea file looks like
-# this,
-#
-#     4 22 8.333 12 103
-#     4 22 8.417 14 116
-#     4 22 8.500 17 131
-#     4 22 8.583 19 144
-#     4 22 8.667 19 151
-#     4 22 8.750 19 161
-#     4 22 8.833 20 169
-#     4 22 8.917 19 175
-#
-# Where the 4th column is direct normal irradiation the the 5th column is
-# diffuse horizontal.
-#
-# Best,
-# Alstan
+        return irrad_beam_nor, irrad_dif
---
+++ tests/test_gen_reindl.py
@@ -0,0 +1,104 @@
+import unittest
+import math
+
+from gen_reindl import *
+
+
+class TestGenReindl(unittest.TestCase):
+
+    def test_calculte_correct_result(self):
+        # results taken from here: http://onebuilding.org/archive/bldg-sim-onebuilding.org/2015-May/046325.html
+
+        # Assume
+
+        lon = -103.98
+        lat = 1.37
+        time_zone = -120
+
+        # Action
+
+        gr = GenReindl(lat, lon, time_zone)
+
+        def round_number(f):
+            return round(f, 0)
+
+        # Assert
+
+        self.assertTrue(round_number(gr.calc_split(4, 22, 8.333333333, 107)[0]) == 12)
+        self.assertTrue(round_number(gr.calc_split(4, 22, 8.333333333, 107)[1]) == 103)
+        self.assertTrue(round_number(gr.calc_split(4, 22, 8.416666667, 121)[0]) == 14)
+        self.assertTrue(round_number(gr.calc_split(4, 22, 8.416666667, 121)[1]) == 116)
+        self.assertTrue(round_number(gr.calc_split(4, 22, 8.5, 137)[0]) == 17)
+        self.assertTrue(round_number(gr.calc_split(4, 22, 8.5, 137)[1]) == 131)
+        self.assertTrue(round_number(gr.calc_split(4, 22, 8.583333333, 151)[0]) == 19)
+        self.assertTrue(round_number(gr.calc_split(4, 22, 8.583333333, 151)[1]) == 144)
+        self.assertTrue(round_number(gr.calc_split(4, 22, 8.666666667, 159)[0]) == 19)
+        self.assertTrue(round_number(gr.calc_split(4, 22, 8.666666667, 159)[1]) == 151)
+        self.assertTrue(round_number(gr.calc_split(4, 22, 8.75, 169)[0]) == 19)
+        self.assertTrue(round_number(gr.calc_split(4, 22, 8.75, 169)[1]) == 161)
+        self.assertTrue(round_number(gr.calc_split(4, 22, 8.833333333, 178)[0]) == 20)
+        self.assertTrue(round_number(gr.calc_split(4, 22, 8.833333333, 178)[1]) == 169)
+        self.assertTrue(round_number(gr.calc_split(4, 22, 8.916666667, 184)[0]) == 19)
+        self.assertTrue(round_number(gr.calc_split(4, 22, 8.916666667, 184)[1]) == 175)
+
+# Original cli docstring
+
+# gen_reindl:
+# Program that transforms global irradiances into orizontal diffuse and direct normal irradiances
+# Note that the -o option has to be specified!
+# Note that the -i option has to be specified!
+#
+# Supported options are:
+# -i       input file [format: month day hour global_irradiation
+# -o       output file [format: month day hour dir_norm_irrad dif_hor_irrad
+# -m       time zone
+# -l       longitude [DEG, West is positive]
+# -a       latitude [DEG, North is positive]
+
+# Further information
+
+# http://onebuilding.org/archive/bldg-sim-onebuilding.org/2015-May/046325.html
+
+# Hi Phil,
+#
+# The gen_reindl program that comes with Daysim <http://daysim.ning.com/>
+# does this pretty well, although it isn't documented on the Daysim
+# website. Below is some info I sent to a student using global horizontal
+# irradiation from our weather station for daylight simulation purposes.
+#
+# First you need to create a tab separated text file of the format *m d
+# h(decimal)    gh_irrad* like the below.
+#
+#     4    22    8.333333333    107
+#     4    22    8.416666667    121
+#     4    22    8.5    137
+#     4    22    8.583333333    151
+#     4    22    8.666666667    159
+#     4    22    8.75    169
+#     4    22    8.833333333    178
+#     4    22    8.916666667    184
+#
+# Then the gen_reindl program can be run. -l is longitude (west positive),
+# -a is latitude (north positive) and -m is the time zone in a multiple of
+# 15 degrees from the meridian. The command below is for Singapore, and
+# you note that it is in the wrong time zone. UTC+8 * 15 = -120, despite a
+# -103.98 longitude.
+#  > gen_reindl -m -120 -l -103.98 -a 1.37 -i input.txt -o output.wea
+#
+# After running the command, the output in the output.wea file looks like
+# this,
+#
+#     4 22 8.333 12 103
+#     4 22 8.417 14 116
+#     4 22 8.500 17 131
+#     4 22 8.583 19 144
+#     4 22 8.667 19 151
+#     4 22 8.750 19 161
+#     4 22 8.833 20 169
+#     4 22 8.917 19 175
+#
+# Where the 4th column is direct normal irradiation the the 5th column is
+# diffuse horizontal.
+#
+# Best,
+# Alstan
