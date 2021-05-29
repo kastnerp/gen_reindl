342a0b598f740ba0ae0d671c04c3565f27f3a4b3
Patrick Kastner <patrick.kastner@gmail.com>
2021-05-29 16:45:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -180,7 +180,7 @@ setup(
     # executes the function `main` from this package when invoked:
     entry_points={  # Optional
         'console_scripts': [
-            'gen_reindl=gen_reindl:main',
+            'gen_reindl=main',
         ],
     },
 
